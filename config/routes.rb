3d05bc5426742d6022309bc5c37ce89209a1afcd
seisyuu <seisyuukodama@gmail.com>
2014-07-09 01:12:03 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 SampleApp::Application.routes.draw do
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  root  'static_pages#home'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  
-
-# You can have the root of your site routed with "root"
-# root 'welcome#index'
-
-
- end
+end
